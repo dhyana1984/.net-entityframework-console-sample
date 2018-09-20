5aed756947be03c57e43ef5dbc5088033107ac81
dhyana1984 <xiongyi_1984@foxmail.com>
2018-09-21 01:08:07 +0800

    添加自定义全局约定类

--- EntityFrameworkSample/Model/EfDbContext.cs
+++ EntityFrameworkSample/Model/EfDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -14,12 +15,22 @@ namespace EntityFrameworkSample.Model
     {
         public EfDbContext():base("name=ConnectionString")
         {
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EfDbContext>());
+            ////数据库不存在就创建
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<EfDbContext>());
+
+            ////总是创建数据库，无论存在与否
+            //Database.SetInitializer(new DropCreateDatabaseAlways<EfDbContext>());
+
+            ////如果EF检测到数据库模型发生了改变，将更新模型
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<EfDbContext>());
         }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-           
-       
+            //全局配置模型的Id作为主键
+            //modelBuilder.Properties().Where(t => t.Name == "Id").Configure(config => config.IsKey());
+
+            //自定义CustomerKeyConvention类作为自定义配置项
+            modelBuilder.Conventions.Add<CustomerKeyConvention>();
 
             //注册Map类
 
@@ -40,5 +51,14 @@ namespace EntityFrameworkSample.Model
         public DbSet<Course> Courses { get; set; }
 
         public DbSet<StudentContract> StudentContracts { get; set; }
+    }
+
+    //自定义全局约定
+    public class CustomerKeyConvention:Convention
+    {
+        public CustomerKeyConvention()
+        {
+            Properties().Where(t => t.Name == "Id").Configure(config => config.IsKey());
+        }
     }
 }
